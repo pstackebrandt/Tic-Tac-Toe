10947aac2cece8d62effbb74d0fc642f16e2915a
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2020-10-10 18:37:14 +0200

    Add Date and time formatting, N-th symbol

--- Problems/N-th symbol/src/Main.kt
+++ Problems/N-th symbol/src/Main.kt
@@ -2,8 +2,8 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    val text = scanner.next()
+    val text = scanner.nextLine()
     val number = scanner.nextInt()
 
-    println("Symbol # $number of the string \"$text\" is '${text[number]}'")
+    println("Symbol # $number of the string \"$text\" is '${text[number - 1]}'")
 }
