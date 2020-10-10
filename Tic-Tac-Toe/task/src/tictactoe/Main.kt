bf53011783f285ae98175474b46942ee2fac9c53
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2020-10-10 19:10:00 +0200

    Add Stage 2/5: The user is the gamemaster

--- Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,11 +1,21 @@
 package tictactoe
 
+import java.util.Scanner
+
 fun main() {
-    printGame()
+    val cells = getInput()
+    printGame(cells)
 }
 
-fun printGame() {
-    print("X O X")
-    print("O X O")
-    print("X X X")
+fun getInput(): String {
+    val scanner = Scanner(System.`in`)
+    return scanner.next()
+}
+
+fun printGame(cells: String) {
+    println("---------")
+    println("| ${cells[0]} ${cells[1]} ${cells[2]} |")
+    println("| ${cells[3]} ${cells[4]} ${cells[5]} |")
+    println("| ${cells[6]} ${cells[7]} ${cells[8]} |")
+    println("---------")
 }
