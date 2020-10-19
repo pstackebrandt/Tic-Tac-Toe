812a74504a9263ca688046328a4284e5d54f1b21
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2020-10-19 19:56:52 +0200

    tic tac toe corrections

--- Problems/The first position of the largest element/src/Main.kt
+++ Problems/The first position of the largest element/src/Main.kt
@@ -1,6 +1,23 @@
 import java.util.*
 
-fun main(args: Array<String>) {
+fun main() {
     val scanner = Scanner(System.`in`)
-    // put your code here
+    with(getNumbers(scanner)) {
+        val max = this.max()
+        val index = getPosition(this, max)
+
+        println("$max $index")
+    }
 }
+
+private fun getNumbers(scanner: Scanner): List<Int> = mutableListOf<Int>()
+        .apply {
+            scanner.let {
+                while (it.hasNextInt()) {
+                    this.add(it.nextInt())
+                }
+            }
+        }
+
+private fun getPosition(numbers: List<Int>, max: Int?) =
+        numbers.indexOf(max) + 1
