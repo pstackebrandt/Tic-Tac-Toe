3dad73b0f561796d634d7bafc56d6e25885afb15
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2020-10-20 13:14:02 +0200

    add The sum of elements

--- Problems/The sum of elements/src/Main.kt
+++ Problems/The sum of elements/src/Main.kt
@@ -1,6 +1,20 @@
 import java.util.*
 
-fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`)
-    // put your code here
+fun main() {
+    println(getNumbers().sum())
+}
+
+private fun getNumbers(): List<Int> {
+    var list = mutableListOf<Int>()
+    with(Scanner(System.`in`)) {
+        while (this.hasNextInt()) {
+            var value = this.nextInt()
+            if (value > 0) {
+                list.add(value)
+            } else {
+                break
+            }
+        }
+    }
+    return list
 }
