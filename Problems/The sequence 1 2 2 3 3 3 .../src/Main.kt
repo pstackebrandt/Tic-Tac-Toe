01b1923d0a2b6973362b80db8e833bb6734d1241
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2020-10-21 14:59:06 +0200

    Add The sequence 1 2 2 3 3 3 ...

--- Problems/The sequence 1 2 2 3 3 3 .../src/Main.kt
+++ Problems/The sequence 1 2 2 3 3 3 .../src/Main.kt
@@ -4,19 +4,21 @@ fun main() {
     getNumbers(getCount()).forEach { print("$it ") }
 }
 
-private fun getNumbers(number: Int) =
-        mutableListOf<Int>().apply {
-            var leftNumbers = number
-            var repetions = 1
-            while (leftNumbers > 0) {
-                for (repetition in 1..repetions) {
-                    if (leftNumbers > 0)
-                        this.add(repetions)
-                    leftNumbers--
+private fun getNumbers(number: Int): MutableList<Int> {
+    return mutableListOf<Int>()
+            .apply {
+                var leftNumbers = number
+                var repetions = 1
+                while (leftNumbers > 0) {
+                    for (repetition in 1..repetions) {
+                        if (leftNumbers > 0) {
+                            add(repetions)
+                        }
+                        leftNumbers--
+                    }
+                    repetions++
                 }
-                repetions++
             }
-        }
-
+}
 
 private fun getCount() = Scanner(System.`in`).nextInt()
